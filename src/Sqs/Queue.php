6f4159c7a02602c8e067bfced96793e32aa67260
milan.kaszas <milan.kaszas@byborgenterprises.com>
2025-06-05 10:51:52 +0200

    SP-2445 chore: laravel 12 compatibility

--- src/Sqs/Queue.php
+++ src/Sqs/Queue.php
@@ -21,7 +21,7 @@ class Queue extends SqsQueue
      * @param  string  $queue
      * @return string
      */
-    protected function createPayload($job, $data = '', $queue = null)
+    protected function createPayload($job, $queue, $data = '', $delay = null)
     {
         if (!$job instanceof DispatcherJob) {
             return parent::createPayload($job, $data, $queue);
